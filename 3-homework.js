3031ed4ca810a1ed1143110b942de2b6f53a503e
Richard Clarke <freshnewrich@gmail.com>
2022-01-13 14:35:28 -0800

    fixed fizz buzz

--- 3-homework.js
+++ 3-homework.js
@@ -174,19 +174,18 @@ function reversingArray() {
 // here we go
 function fizzBuzz() {
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 == 0) {
+        if (i % 15 == 0) {
+            console.log("Fizz Buzz!")
+        } else if (i % 3 == 0) {
             console.log("Fizz!")
         } else if (i % 5 == 0) {
-            console.log("Buzz!")
-            //i almost forgot to do 15
-        } else if (i % 15 == 0) {
-            console.log("Fizz Buzz!")
+            console.log("Buzz!") 
         } else {
             console.log(i)
         }  //lets hope this works
     }
 }
-// fizzBuzz()
+fizzBuzz()
 // ! I DID IT!!!!! YEAAAAAAA
 
 
