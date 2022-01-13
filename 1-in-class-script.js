f803d7797676ee670f64c6cbee6dd03981b7ffd0
Richard Clarke <freshnewrich@gmail.com>
2022-01-12 19:35:24 -0800

    forgot something, animal exercise done

--- 1-in-class-script.js
+++ 1-in-class-script.js
@@ -88,7 +88,7 @@
 // Within your function, create a for loop that logs “Mommy, I want to see [insert animal name here]! Waaah!”
 // With your array (and - if needed - with your knowledge of parameters), invoke your function to ensure it is working correctly.
 
-var animals = ["Lion", "Tiger", "Bear", "Elephant"]
+var animals = ["Lemur", "Meerkat", "Eagle", "Rattlesnake"]
 
 function nameAnimals() {
     for (let i = 0; i <= animals.length; i++) {
@@ -97,6 +97,7 @@ function nameAnimals() {
 }
 
 //nameAnimals()
+
 
 // ?     Part 2:
 //     Create a function that dynamically adds the following animal names to the current array. You must NOT recreate the array, only add values.
@@ -112,18 +113,49 @@ function nameAnimals() {
 // In the function that is printing out, add an if statement. If the animal name is longer than 10 characters, say “Mommy, that animal name is too long to pronounce :(“, Otherwise print the other message
 // Make sure the code is re-usable for both adding the names, iterating through the array, and printing the outputs. Avoid duplicate code.
 
-    var newAnimals = ["Tiger",
-        "Wunderpus photogenicus",
-        "Bear",
-        "Pink Fairy Armadillo",
-        "Coyote",
-        "Rasberry Crazy Ant",
-        "Moose",
-        "Satanic Leaf-Tailed Gecko",
-        "Lion"]
 
-    var newRepeats = newAnimals.length
 
-    for (let i = 0; i < newRepeats; i++) {
-        animals.push(newAnimals.shift())
+//creates an array with the new animal names
+var newAnimals = ["Tiger",
+    "Wunderpus photogenicus",
+    "Bear",
+    "Pink Fairy Armadillo",
+    "Coyote",
+    "Rasberry Crazy Ant",
+    "Moose",
+    "Satanic Leaf-Tailed Gecko",
+    "Lion"]
+
+//counts newAnimals and sets a variable used for iterations of the shift/push for
+var newRepeats = newAnimals.length
+
+//removes the newAnimals one at a time and adds them to animals one at a time
+for (let i = 0; i < newRepeats; i++) {
+    animals.push(newAnimals.shift())
+}
+
+// there is another way to do it all at once by combining the two arrays
+var combinedAnimals = [...animals, ...newAnimals]
+
+//unpronounceable names function
+function nameAnimalsUnpronounceable() {
+    for (let i = 0; i < animals.length; i++) {
+        if (animals[i].length < 10) {
+            console.log("Mommy, I want to see " + animals[i] + "! Waaah!");
+        } else {
+            console.log("Mommy, that animal name is too long to pronounce :(")
+        }
     }
+}
+
+
+nameAnimalsUnpronounceable()
+
+// console.log(animals.length + "is the number of values in the array")
+// console.log(animals[0])
+// console.log(animals[0].length)
+// console.log(animals[1])
+// console.log(animals[1].length)
+
+
+
