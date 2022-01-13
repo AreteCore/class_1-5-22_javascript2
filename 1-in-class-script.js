599fdab95422c7ebdb38a577797881a3eb5cd5c5
Richard Clarke <freshnewrich@gmail.com>
2022-01-12 18:48:52 -0800

    arrays, loops, animal exercise

--- 1-in-class-script.js
+++ 1-in-class-script.js
@@ -4,33 +4,33 @@
 // In the else statement, console log “I feel fantastic?”
 // Run the code a few times, and change the hoursOfSleep variable to test different results.
 
-console.log("exercise 1 start")
+// console.log("exercise 1 start")
 
-var hoursOfSleep = 4
+// var hoursOfSleep = 4
 
-function howMuchSleep(){
-    if (hoursOfSleep <= 6) {
-        console.log("I am groggy. " + hoursOfSleep + " hours.")
-    } else {
-        console.log("I feel fantastic." + hoursOfSleep + " hours.")
-    }
-}                  
+// function howMuchSleep(){
+//     if (hoursOfSleep <= 6) {
+//         console.log("I am groggy. " + hoursOfSleep + " hours.")
+//     } else {
+//         console.log("I feel fantastic." + hoursOfSleep + " hours.")
+//     }
+// }                  
 
-howMuchSleep()
+// howMuchSleep()
 
-hoursOfSleep += 1
+// hoursOfSleep += 1
 
-howMuchSleep()
+// howMuchSleep()
 
-hoursOfSleep += 1
+// hoursOfSleep += 1
 
-howMuchSleep()
+// howMuchSleep()
 
-hoursOfSleep += 1
+// hoursOfSleep += 1
 
-howMuchSleep()
+// howMuchSleep()
 
-hoursOfSleep += 1
+// hoursOfSleep += 1
 
 
 
@@ -43,14 +43,87 @@ hoursOfSleep += 1
 // Write an else if statement that if the nextLocation is equal to work, write to the screen “Good morning, finding the fastest route to work!”
 // Write an else statement that writes to the screen “Good morning, finding the fastest route to work!”
 // Change the nextLocation to different inputs to change the if statement.
-console.log("exercise 2 start")
+// console.log("exercise 2 start")
 
-var nextLocation = "asdf"
+// var nextLocation = "asdf"
 
-if (nextLocation == "home") {
-    console.log("It’s been a long day, let’s go home!")
-} else if (nextLocation == "work") {
-    console.log("Good morning, finding the fastest route to work!")
-} else {
-    console.log("idk")
+// if (nextLocation == "home") {
+//     console.log("It’s been a long day, let’s go home!")
+// } else if (nextLocation == "work") {
+//     console.log("Good morning, finding the fastest route to work!")
+// } else {
+//     console.log("idk")
+// }
+
+
+
+// ? for loops
+//starting point
+// terminating point
+//increment
+
+// for (var i = 0; i <= 10; i++) {
+//     console.log("Count " + i)
+// }
+
+
+// var string = "testing"
+// for (var i = 0; i < string.length; i++) {
+//     console.log("Letter is " + string.charAt(i))
+// }
+
+// console.log(string.length)
+
+//how do i print the last letter
+
+//string.length?
+
+// console.log(string.charAt(string.length - 1))    
+
+
+// ? in class exercise    
+// ?     Part 1:
+// Create an array of four animals called animals.
+// Create a function called nameAnimals.
+// Within your function, create a for loop that logs “Mommy, I want to see [insert animal name here]! Waaah!”
+// With your array (and - if needed - with your knowledge of parameters), invoke your function to ensure it is working correctly.
+
+var animals = ["Lion", "Tiger", "Bear", "Elephant"]
+
+function nameAnimals() {
+    for (let i = 0; i <= animals.length; i++) {
+        console.log("Mommy, I want to see " + animals[i] + "! Waaah!");
+    }
 }
+
+//nameAnimals()
+
+// ?     Part 2:
+//     Create a function that dynamically adds the following animal names to the current array. You must NOT recreate the array, only add values.
+// Tiger
+// Wunderpus photogenicus
+// Bear
+// Pink Fairy Armadillo
+// Coyote
+// Rasberry Crazy Ant
+// Moose
+// Satanic Leaf-Tailed Gecko
+// Lion
+// In the function that is printing out, add an if statement. If the animal name is longer than 10 characters, say “Mommy, that animal name is too long to pronounce :(“, Otherwise print the other message
+// Make sure the code is re-usable for both adding the names, iterating through the array, and printing the outputs. Avoid duplicate code.
+
+    var newAnimals = ["Tiger",
+        "Wunderpus photogenicus",
+        "Bear",
+        "Pink Fairy Armadillo",
+        "Coyote",
+        "Rasberry Crazy Ant",
+        "Moose",
+        "Satanic Leaf-Tailed Gecko",
+        "Lion"]
+
+    var newRepeats = newAnimals.length
+
+    for (let i = 0; i < newRepeats; i++) {
+        animals.push(newAnimals.shift())
+    }
